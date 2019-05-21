2872c508d7866d11c6603623eba1a0e5a40ae1f8
pallavikandala <50877909+pallavikandala@users.noreply.github.com>
2019-05-21 22:10:38 +0530

    Update employee.java

--- employee.java
+++ employee.java
@@ -1,1 +1,32 @@
 
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Employee
+{
+private String name;
+private int salary;
+
+public void init(String n,int sal){
+    name = n;
+    salary = sal;
+    
+}
+public void setSalary(int sal){
+    salary=sal;
+    
+}
+public void getSalary(){
+    return salary;
+    
+}
+public void print(){
+    System.out.println(name);
+    System.out.println(salary);
+}
+}
